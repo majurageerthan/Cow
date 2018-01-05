5179932b0b29d0660b6fdf2d6168b0eb3e133b40
majurageerthan <majuran.thas@gmail.com>
2018-01-06 00:37:49 +0530

    WebSite Updated

--- src/main/java/com/shankeerthan/WebSite.java
+++ src/main/java/com/shankeerthan/WebSite.java
@@ -11,7 +11,7 @@ public class WebSite {
 
     public static void open() {
         try {
-            URI u = new URI("https://google.com");
+            URI u = new URI("https://happycow.000webhostapp.com/");
             java.awt.Desktop.getDesktop().browse(u);
 
         } catch (Exception e) {
